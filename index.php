dab893bfa1bd1a5ead51917c839e8f5346fe71cb
prateekagarwal2012 <32671132+prateekagarwal2012@users.noreply.github.com>
2017-10-10 13:32:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 echo "<h1>Hello World VERSION0.2</h1>";
 echo "<p>Hello World from heroku</p>";
 echo "<p>This is a new paragraph</p>";
-echo "<p><a href="peopleapi.php">Link to People API</a></p>";
+echo "<p><a href='peopleapi.php'>Link to People API</a></p>";
